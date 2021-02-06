f5be5ceaa035fc357b5c6d1d0d1c079bf3089202
Rphad <raphael.deknop@ulb.be>
2021-02-07 00:10:38 +0100

    added light theme switch in mobile menu. Time to tidy all the css now

--- js/mobile.js
+++ js/mobile.js
@@ -2,20 +2,18 @@
 because it is easier and because the height of the topnav
 is determined by the height of the logotext + its padding*/
 
-let logo = document.getElementsByClassName('topnav')[0]
-let logoheightval = parseFloat(getComputedStyle(logo).height)
+var logo = document.getElementsByClassName('topnav')[0]
+var logoheightval = parseFloat(getComputedStyle(logo).height)
 
-let navcontainer = document.getElementsByClassName('container')[0]
+var navcontainer = document.getElementsByClassName('container')[0]
 /*here the 3 bars are 2px high, and they have a margin of 10px*/
-let tbpadding = (logoheightval - 3*2 - 4*10)/2
-let allpadding = tbpadding+"px 0px"
+var tbpadding = (logoheightval - 3*2 - 4*10)/2
+var allpadding = tbpadding+"px 0px"
 
 navcontainer.style.padding = allpadding
 
 function MobileMenu(x) {
   x.classList.toggle("open");
-  const all = document.querySelector('*');
-  var all_class = all.classList;
   var sidepanel = document.getElementById("mySidepanel");
   var sidelist = sidepanel.classList;
   if (! sidelist.contains("open")) {
