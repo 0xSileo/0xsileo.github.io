9fb07d0428c8add0bf32097dde1a813e880773be
Rphad <raphael.deknop@ulb.be>
2021-05-20 19:16:00 +0200

    minor

--- js/en_donations.js
+++ js/en_donations.js
@@ -1,79 +1,76 @@
 function cryptoChange(selection) {
-	var coinproperties = JSON.parse(selection.value);
-	document.getElementById('crypto-qr').src = coinproperties.imageurl;
-	document.getElementById('CoinDescription').innerHTML = coinproperties.description;
-	document.getElementById('address').innerHTML = coinproperties.address;
+  var coinproperties = JSON.parse(selection.value);
+  document.getElementById('crypto-qr').src = coinproperties.imageurl;
+  document.getElementById('CoinDescription').innerHTML = coinproperties.description;
+  document.getElementById('address').innerHTML = coinproperties.address;
 }
-
 
 const metamaskDiv = document.querySelector('#forMetamask');
 
 if (typeof window.ethereum !== 'undefined') {
   console.log('MetaMask is installed!');
-	/*Displaying the div*/
-	metamaskDiv.style.display = "";
-	var idToExplorer = {
-		"0x1": function(hash){return "https://www.etherscan.io/tx/" + hash},
+  /*Displaying the div*/
+  metamaskDiv.style.display = "";
+  var idToExplorer = {
+    "0x1": function(hash){return "https://www.etherscan.io/tx/" + hash},
     "0x3": function(hash){return "https://ropsten.etherscan.io/tx/" + hash},
-		"0x4": function(hash){return "https://rinkeby.etherscan.io/tx/" + hash},
-		"0x5": function(hash){return "https://goerli.etherscan.io/tx/" + hash},
-		"0x2a": function(hash){return "https://kovan.etherscan.io/tx/" + hash}
-	}
+    "0x4": function(hash){return "https://rinkeby.etherscan.io/tx/" + hash},
+    "0x5": function(hash){return "https://goerli.etherscan.io/tx/" + hash},
+    "0x2a": function(hash){return "https://kovan.etherscan.io/tx/" + hash}
+  }
+  
   let accounts;
-	const chainP = document.querySelector('#networkChainP')
+  const chainP = document.querySelector('#networkChainP')
   const statusP = document.querySelector('#connexionStatusP');
-	const ethereumButton = document.querySelector('.enableEthereumButton');
-	const sendEthButton = document.querySelector('.sendEthButton');
-	const donation = document.querySelector('#Donation');
-	let gasURL = "https://ethgasstation.info/api/ethgasAPI.json";
-	let chainsURL ="https://chainid.network/chains.json";
-	/*Giving a default gas value in case the json doesn't parse.
-		It is customizable in Metamask.*/
-	var gasEsti = 75*10**9;
-	var chains;
+  const ethereumButton = document.querySelector('.enableEthereumButton');
+  const sendEthButton = document.querySelector('.sendEthButton');
+  const donation = document.querySelector('#Donation');
+  let gasURL = "https://ethgasstation.info/api/ethgasAPI.json";
+  let chainsURL ="https://chainid.network/chains.json";
+  /*Giving a default gas value in case the json doesn't parse.
+   It is customizable in Metamask.*/
+  var gasEsti = 75*10**9;
+  var chains;
 
   var i = 0;
-	getGasEstimation();
-	getChains();
-
-	getAccount();
-
-	getNet();
-
-	ethereumButton.addEventListener('click', () => {
-	  getAccount();
-	});
+  getGasEstimation();
+  getChains();
+  getAccount();
+  getNet();
+	
+  ethereumButton.addEventListener('click', () => {
+    getAccount();
+  });
 
   function checkNetwork() {
     currentChainId = parseInt(ethereum.chainId, 16);
-		if (currentChainId != 1) {
-			try {
-				for (var i=0; i<chains.length; i++) {
-		    	if (currentChainId == chains[i].chainId) {
-		        chainP.innerHTML = 'You are using ' + chains[i].name + '. Please change to Ethereum Mainnet';
-		    	}
-				}
-			} catch {
-				chainP.innerHTML = 'Make sure you are using the Ethereum Mainnet';
-			}
-		} else {
-			chainP.innerHTML = 'You are using the Ethereum Mainnet';
-		}
+    if (currentChainId != 1) {
+      try {
+        for (var i=0; i<chains.length; i++) {
+          if (currentChainId == chains[i].chainId) {
+	    chainP.innerHTML = 'You are using ' + chains[i].name + '. Please change to Ethereum Mainnet';
+	  }
 	}
+      } catch {
+	chainP.innerHTML = 'Make sure you are using the Ethereum Mainnet';
+	}
+    } else {
+	chainP.innerHTML = 'You are using the Ethereum Mainnet';
+    }
+  }
+  
+  ethereum.on('chainChanged', (chainId) => {
+  // Handle the new chain.
+  // Correctly handling chain changes can be complicated.
+  // We recommend reloading the page unless you have good reason not to.
+    window.location.reload();
+    i++;
+  });
 
-
-	ethereum.on('chainChanged', (chainId) => {
-	  // Handle the new chain.
-	  // Correctly handling chain changes can be complicated.
-	  // We recommend reloading the page unless you have good reason not to.
-		window.location.reload();
-		i++;
-	});
-
-	ethereum.on('accountsChanged', function (accounts) {
-	  window.location.reload();
-		i++
-	});
+  ethereum.on('accountsChanged', function (accounts) {
+    window.location.reload();
+    i++
+  });
 
 	//Sending Ethereum to an address
 	sendEthButton.addEventListener('click', () => {
